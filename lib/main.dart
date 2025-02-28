867590e87cf11aaa425beef7a7464642fc531b9a
tim12367 <tim12367@ymail.com>
2025-02-28 22:30:14 +0800

    客製化主題

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,22 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.purple,
+        ),
+        appBarTheme: AppBarTheme(
+          // titleTextStyle: TextStyle(
+          //   fontWeight: FontWeight.w500,
+          //   fontSize: 20,
+          // ),
+          color: Color(0xFF0f1427),
+          // elevation: 4.0,
+          // shadowColor: Theme.of(context).colorScheme.shadow,
+        ),
+        scaffoldBackgroundColor: Color(0xFF0f1427),
+      ),
       home: InputPage(),
     );
   }
@@ -21,12 +37,14 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true, // 置中
         title: Text('BMI CALCULATOR'),
       ),
       body: Center(
         child: Text('Body Text'),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
